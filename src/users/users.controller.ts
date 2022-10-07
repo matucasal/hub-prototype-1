3e52cac16b476bf560a870ef68590647e16a9bd4
matucasal <matias.casal@yahoo.com.ar>
2022-10-07 10:13:48 -0300

    adding exception to the create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { User as UserModel } from '@prisma/client';
+import { Prisma, User as UserModel } from '@prisma/client';
 
 @Controller('/users')
 export class UsersController {
@@ -9,6 +15,27 @@ export class UsersController {
 
   @Post('/')
   async signupUser(@Body() userData: CreateUserDto): Promise<UserModel> {
-    return this.usersService.createUser(userData);
+    try {
+      const user = await this.usersService.createUser(userData);
+      return user;
+    } catch (err) {
+      console.log('err', err);
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        // The .code property can be accessed in a type-safe manner
+        if (err.code === 'P2002') {
+          console.log(
+            'There is a unique constraint violation, a new user cannot be created with this userwallet',
+          );
+        }
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error:
+            'There is a unique constraint violation, a new user cannot be created with this userwallet',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
